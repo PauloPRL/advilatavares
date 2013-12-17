f12601ad6a497e7b7af1dd17ca051d73459eba6d
Carlos Maniero <carlosmaniero@gmail.com>
2013-12-17 18:56:32 -0200

    Conteúdo do Bottom

--- apps/linked_content/models.py
+++ apps/linked_content/models.py
@@ -7,21 +7,31 @@ from mezzanine.pages.models import Page
 
 class LinkedContent(models.Model):
     key = models.CharField(max_length=16, unique=True, null=False, blank=False)
-    page = models.ForeignKey(Page, verbose_name="Página a ser linkada", null=True)
+    page = models.ForeignKey(Page, verbose_name="Página a ser linkada", null=True, blank=True)
     _title = models.CharField(max_length=255, null=True, blank=True, verbose_name="Título")
     _description = RichTextField(null=True, blank=True, verbose_name="Descrição")
     button = models.CharField(max_length=20, verbose_name="Título do botão", default="Clique aqui")
 
     @property
     def title(self):
-        return self._title or self.page.title
+        if self._title:
+            return self._title
+        elif self.page:
+            return self.page.title
+        else:
+            return "Sem Título"
 
     @property
     def description(self):
-        return self._description or self.page.description
+        if self._description:
+            return self._description
+        elif self.page:
+            return self.page.description
+        else:
+            return "--"
 
     class Meta:
-        verbose_name='Conteúdos linkável'
+        verbose_name = 'Conteúdos linkável'
 
     def __unicode__(self):
         return self.key
