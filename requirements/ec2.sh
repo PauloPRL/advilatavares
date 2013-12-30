4c5383413b06b12da3bd8efbdb6b2caf3399f54e
Carlos Maniero <carlosmaniero@gmail.com>
2013-12-30 09:29:13 -0200

    Arquivos para serem instalados no ec2

--- requirements/ec2.sh
+++ requirements/ec2.sh
@@ -3,10 +3,10 @@ apt-get install python-pip
 apt-get install libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev
 
 echo "Configure VirutalEnvWrapper? [y/n]"
-  pip install virtualenvwrapper
-  echo "export WORKON_HOME=~/Envs
+pip install virtualenvwrapper
+echo "export WORKON_HOME=~/Envs
 mkdir -p $WORKON_HOME
 source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
-  export WORKON_HOME=~/Envs
-  mkdir -p $WORKON_HOME
-  source /usr/local/bin/virtualenvwrapper.sh
+export WORKON_HOME=~/Envs
+mkdir -p $WORKON_HOME
+source /usr/local/bin/virtualenvwrapper.sh
