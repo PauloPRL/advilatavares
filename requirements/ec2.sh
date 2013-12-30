3ab6c75451b26d8000ed2b59d4e146518a2a8366
Carlos Maniero <carlosmaniero@gmail.com>
2013-12-30 09:34:04 -0200

    Arquivos para serem instalados no ec2

--- requirements/ec2.sh
+++ requirements/ec2.sh
@@ -1,6 +1,6 @@
 apt-get install git
 apt-get install python-pip
-apt-get install libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev
+apt-get install libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev python-dev
 
 echo "Configure VirutalEnvWrapper? [y/n]"
 pip install virtualenvwrapper
