11b55b2f35d5608d98a2b40ddeb914f6988b1735
Carlos Maniero <carlosmaniero@gmail.com>
2013-12-31 01:13:40 -0200

    gunicorn config

--- deploy/gunicorn.sh
+++ deploy/gunicorn.sh
@@ -2,7 +2,7 @@
  
 NAME="advilatavares" # Name of the application
 DJANGODIR=/home/ubuntu/djago_projects/advilatavares # Django project directory
-SOCKFILE=//home/ubuntu/djago_projects/advilatavares/deploy/gunicorn.sock # we will communicte using this unix socket
+SOCKFILE=/home/ubuntu/djago_projects/advilatavares/deploy/gunicorn.sock # we will communicte using this unix socket
 NUM_WORKERS=3 # how many worker processes should Gunicorn spawn
 DJANGO_SETTINGS_MODULE=advilatavares.settings # which settings file should Django use
 DJANGO_WSGI_MODULE=advilatavares.wsgi # WSGI module name
@@ -11,7 +11,7 @@ echo "Starting $NAME as `whoami`"
  
 # Activate the virtual environment
 cd $DJANGODIR
-source ~/Envs/advilatavares/bin/activate
+source /home/ubuntu/Envs/advilatavares/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
  
