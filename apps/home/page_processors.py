1eb25e01b72c230089cfee71f7153041594fc193
Carlos Maniero <carlosmaniero@gmail.com>
2014-02-02 09:21:00 -0200

    Posts na home

--- apps/home/page_processors.py
+++ apps/home/page_processors.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from time import sleep
 from mezzanine.blog.models import BlogPost
 from mezzanine.pages.page_processors import processor_for
 
@@ -6,4 +7,5 @@ from mezzanine.pages.page_processors import processor_for
 @processor_for('/')
 def home(request, page):
     posts = BlogPost.objects.filter(status=2).order_by()[0:5]
+    sleep(2)
     return {'blog_posts': posts}
