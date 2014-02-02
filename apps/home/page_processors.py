e413fbc7c8c95fcb6ac3b19b50e864c341e15fa6
Carlos Maniero <carlosmaniero@gmail.com>
2014-02-02 09:18:21 -0200

    Posts na home

--- apps/home/page_processors.py
+++ apps/home/page_processors.py
@@ -6,4 +6,4 @@ from mezzanine.pages.page_processors import processor_for
 @processor_for('/')
 def home(request, page):
     posts = BlogPost.objects.filter(status=2).order_by()[0:5]
-    return {'posts': posts}
+    return {'blog_posts': posts}
