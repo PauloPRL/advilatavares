39c9b7d8186ddbc157cac48b773adf78a5b69c62
Carlos Maniero <carlosmaniero@gmail.com>
2014-02-02 09:36:05 -0200

    Facebook plugin

--- apps/home/page_processors.py
+++ apps/home/page_processors.py
@@ -5,7 +5,7 @@ from mezzanine.pages.page_processors import processor_for
 
 @processor_for('/')
 def home(request, page):
-    posts = BlogPost.objects.filter(status=2).order_by()[0:5]
+    posts = BlogPost.objects.filter(status=2).order_by()[0:3]
     print('-' * 30)
     print posts
 
