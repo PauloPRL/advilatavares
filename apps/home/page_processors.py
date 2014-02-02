2b46bc6dc058a9a980200afbe290213c75313194
Carlos Maniero <carlosmaniero@gmail.com>
2014-02-02 08:56:34 -0200

    Posts na home

--- apps/home/page_processors.py
+++ apps/home/page_processors.py
@@ -1,20 +1,9 @@
+from __future__ import unicode_literals
+from mezzanine.blog.models import BlogPost
 from mezzanine.pages.page_processors import processor_for
-from ..agenda.models import Event
-from ..agenda.utils import get_days_in_week
 
 
 @processor_for('/')
 def home(request, page):
-    days = get_days_in_week()
-    events = Event.objects.filter(date__range=(days[0], days[-1]))
-
-    agenda = []
-    for day in days:
-        tmp = {'date': day, 'events': []}
-        for event in events:
-            if event.date.day == day.day:
-                tmp['events'].append(event)
-
-        agenda.append(tmp)
-
-    return {'agenda': agenda}
+    posts = BlogPost.objects.filter(status=2).order_by()[0:5]
+    return {'posts': posts}
