f7cc232209abaf4a607c12d1526790cbdb407e49
Carlos Maniero <carlosmaniero@gmail.com>
2014-02-02 00:06:19 -0200

    Configuração do template de agenda

---
+++ apps/agenda/page_processors.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+from __future__ import unicode_literals
+import datetime
+from django.utils.timezone import now
+from mezzanine.pages.page_processors import processor_for
+from .models import Agenda, Event
+from dateutil.relativedelta import relativedelta
+
+
+@processor_for(Agenda)
+def agenda(request, page):
+    today = now()
+    month = int(request.GET.get('month', 0))
+    date = today + relativedelta(months=month)
+
+
+    return {'events': Event.objects.filter(agenda=page.agenda, date__year=date.year, date__month=date.month),
+            'date':date, 'month': month}
